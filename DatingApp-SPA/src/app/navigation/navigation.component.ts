30bba6d88edb654bf87b515b466838392a4a6696
Adam Stevens <astevens1118@gmail.com>
2019-08-19 17:28:01 -0400

    created login and register in client

---
+++ DatingApp-SPA/src/app/navigation/navigation.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  Login() {
+    this.authService.Login(this.model).subscribe(
+      next => {
+        console.log('Logged in Successfully'); }, error => {
+          console.log('Failed to Log In');
+        });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  Logout() {
+    localStorage.removeItem('token');
+  }
+
+}
